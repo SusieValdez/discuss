3aba800e679410b6693ee69e6fb1fc4bf11b1b60
SusieHatter <susiivaldez@gmail.com>
2021-11-29 20:57:49 -0600

    Mock out members sidebar with styling

--- client/src/components/UsersSidebar/UsersSidebar.styles.js
+++ client/src/components/UsersSidebar/UsersSidebar.styles.js
@@ -6,4 +6,69 @@ export const Container = styled.div`
   min-width: 240px;
   background-color: #2f3136;
   flex-direction: column;
+  padding-left: 8px;
+
+  h2 {
+    color: #8e9297;
+    font-family: var(--font-display);
+    padding: 24px 8px 16px;
+    height: 40px;
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: 0.25px;
+    font-weight: 600;
+    line-height: 16px;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const UserContainer = styled.div`
+  height: 42px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 12px;
+  border-radius: 4px;
+  padding: 0 8px;
+
+  :hover {
+    background-color: rgba(79, 84, 92, 0.32);
+  }
+`;
+
+export const ProfileImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  flex: 0 0 auto;
+  margin-right: 12px;
+`;
+
+export const UserContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 225px;
+`;
+
+export const Username = styled.span`
+  color: rgb(210, 14, 211);
+  justify-content: flex-start;
+  align-items: center;
+  display: flex;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 500;
+  flex: 0 1 auto;
+`;
+
+export const Legend = styled.div`
+  font-weight: 500;
+  color: #8e9297;
+  line-height: 16px;
+  font-size: 12px;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
--- client/src/components/UsersSidebar/index.js
+++ client/src/components/UsersSidebar/index.js
@@ -1,9 +1,73 @@
 import React from "react";
 // Styles
-import { Container } from "./UsersSidebar.styles";
+import {
+  Container,
+  UserContainer,
+  ProfileImage,
+  UserContent,
+  Username,
+  Legend,
+} from "./UsersSidebar.styles";
 
 const UsersSidebar = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <h2>Server President</h2>
+      <User
+        username="Prima"
+        avatarUrl="https://i.pravatar.cc/300?u=1"
+        legend="hey, ho... "
+      />
+      <User
+        username="Louie"
+        avatarUrl="https://i.pravatar.cc/300?u=2"
+        legend="More Movies... "
+      />
+      <User
+        username="Mirna"
+        avatarUrl="https://i.pravatar.cc/300?u=3"
+        legend="I like waffles "
+      />
+      <User
+        username="Linda"
+        avatarUrl="https://i.pravatar.cc/300?u=4"
+        legend="Aliens👽 "
+      />
+      <h2>Members</h2>
+      <User
+        username="Tobey"
+        avatarUrl="https://i.pravatar.cc/300?u=5"
+        legend="Normalize pets"
+      />
+      <User
+        username="Sonya"
+        avatarUrl="https://i.pravatar.cc/300?u=6"
+        legend="Found my glasses!"
+      />
+      <User
+        username="Tina"
+        avatarUrl="https://i.pravatar.cc/300?u=7"
+        legend="I'm cold teehee"
+      />
+      <User
+        username="Tim"
+        avatarUrl="https://i.pravatar.cc/300?u=8"
+        legend="Mom's fave pic"
+      />
+    </Container>
+  );
+};
+
+const User = ({ avatarUrl, username, legend }) => {
+  return (
+    <UserContainer>
+      <ProfileImage src={avatarUrl} />
+      <UserContent>
+        <Username>{username}</Username>
+        <Legend>{legend} </Legend>
+      </UserContent>
+    </UserContainer>
+  );
 };
 
 export default UsersSidebar;
