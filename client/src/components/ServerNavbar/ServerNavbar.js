cc598fa6efed835936e2a3caeea2efce2651533e
SusieHatter <susiivaldez@gmail.com>
2022-01-13 15:52:53 -0600

    Separate home icon from server icons 🪓

--- client/src/components/ServerNavbar/ServerNavbar.js
+++ client/src/components/ServerNavbar/ServerNavbar.js
@@ -5,27 +5,31 @@ import { Container, Item, Separator, Icon } from "./ServerNavbar.styles";
 const ServerNavbar = () => {
   return (
     <Container>
-      <Item>
-        <Icon>
-          <img src="https://picsum.photos/48?random=1" alt="" />
-        </Icon>
-      </Item>
+      <div>
+        <Item>
+          <Icon>
+            <span>H</span>
+          </Icon>
+        </Item>
+      </div>
       <Separator />
-      <Item>
-        <Icon>
-          <img src="https://picsum.photos/48?random=2" alt="" />
-        </Icon>
-      </Item>
-      <Item>
-        <Icon>
-          <img src="https://picsum.photos/48?random=3" alt="" />
-        </Icon>
-      </Item>
-      <Item>
-        <Icon>
-          <img src="https://picsum.photos/48?random=4" alt="" />
-        </Icon>
-      </Item>
+      <div>
+        <Item>
+          <Icon>
+            <img src="https://picsum.photos/48?random=2" alt="" />
+          </Icon>
+        </Item>
+        <Item>
+          <Icon>
+            <img src="https://picsum.photos/48?random=3" alt="" />
+          </Icon>
+        </Item>
+        <Item>
+          <Icon>
+            <img src="https://picsum.photos/48?random=4" alt="" />
+          </Icon>
+        </Item>
+      </div>
     </Container>
   );
 };
--- client/src/components/ServerNavbar/ServerNavbar.styles.js
+++ client/src/components/ServerNavbar/ServerNavbar.styles.js
@@ -23,11 +23,25 @@ export const Separator = styled.div`
 `;
 
 export const Icon = styled.div`
+  display: flex;
   width: 48px;
   height: 48px;
   border-radius: 50%;
   margin: 10px auto;
   background-color: #36393f;
+  transition: all 0.15s ease-out;
+  align-items: center;
+  justify-content: center;
+  color: #8e9297;
+
+  :hover {
+    border-radius: 18px;
+    color: #fff;
+  }
+
+  span {
+    font-size: 32px;
+  }
 
   img {
     max-width: 48px;
