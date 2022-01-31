2bfbdd4fa958ee6eef05d74149b408766acf09f8
SusieHatter <susiivaldez@gmail.com>
2022-01-30 23:26:15 -0600

    Add channel and category menus

--- client/src/components/Sidebar/ChannelCategory.js
+++ client/src/components/Sidebar/ChannelCategory.js
@@ -3,14 +3,31 @@ import ChannelTitle from "./ChannelTitle";
 import { Category } from "./ChannelCategory.styles";
 import chevronDown from "../../assets/chevron-down-solid.svg";
 import chevronRight from "../../assets/chevron-right-solid.svg";
+import { MenuItem, useMenuState } from "@szhsin/react-menu";
+import { Menu } from "../../ui/Menus";
 
 const ChannelCategory = ({ name, channels, activeChannel }) => {
   const [showChannels, setShowChannels] = useState(true);
   const toggleShowChannels = () => setShowChannels(!showChannels);
   const isActive = (channel) => channel._id === activeChannel._id;
+
+  const categoryMenu = useMenuState();
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+
+  const onRightClickCategory = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setAnchorPoint({ x: e.clientX, y: e.clientY });
+    categoryMenu.toggleMenu(true);
+  };
+
   return (
     <Category>
-      <div className="category-header" onClick={toggleShowChannels}>
+      <div
+        className="category-header"
+        onClick={toggleShowChannels}
+        onContextMenu={onRightClickCategory}
+      >
         <img src={showChannels ? chevronDown : chevronRight} alt="" />
         {name}
       </div>
@@ -25,6 +42,15 @@ const ChannelCategory = ({ name, channels, activeChannel }) => {
           )
         )}
       </div>
+      <Menu
+        state={categoryMenu.state}
+        endTransition={categoryMenu.endTransition}
+        anchorPoint={anchorPoint}
+        onClose={() => categoryMenu.toggleMenu(false)}
+      >
+        <MenuItem>Edit Category</MenuItem>
+        <MenuItem>Delete Category</MenuItem>
+      </Menu>
     </Category>
   );
 };
--- client/src/components/Sidebar/ChannelTitle.js
+++ client/src/components/Sidebar/ChannelTitle.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Title } from "./ChannelTitle.styles.js";
 
 import hashtag from "../../assets/hashtag-solid.svg";
 import { Link, useParams } from "react-router-dom";
+import { MenuItem, useMenuState } from "@szhsin/react-menu";
+import { Menu } from "../../ui/Menus.js";
 
 const ChannelTitle = ({ _id, name, isActive }) => {
   const { serverId } = useParams();
+
+  const categoryMenu = useMenuState();
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+
+  const onRightClickCategory = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setAnchorPoint({ x: e.clientX, y: e.clientY });
+    categoryMenu.toggleMenu(true);
+  };
+
   return (
     <Link to={`/servers/${serverId}/channels/${_id}`}>
-      <Title isActive={isActive}>
+      <Title isActive={isActive} onContextMenu={onRightClickCategory}>
         <img src={hashtag} alt="" />
         {name}
       </Title>
+      <Menu
+        state={categoryMenu.state}
+        endTransition={categoryMenu.endTransition}
+        anchorPoint={anchorPoint}
+        onClose={() => categoryMenu.toggleMenu(false)}
+      >
+        <MenuItem>Edit Channel</MenuItem>
+        <MenuItem>Delete Channel</MenuItem>
+      </Menu>
     </Link>
   );
 };
