2386970e26b0d419e03e7e4e13a2e3ce9c830c25
SusieHatter <susiivaldez@gmail.com>
2022-02-08 07:35:17 -0600

    Add roles window to serversetting's modal with styling

---
+++ client/src/components/ServerSettingsModal/Roles.js
@@ -0,0 +1,76 @@
+import React from "react";
+// Styles
+import {
+  Container,
+  Content,
+  DefaultPermissions,
+  CreateNewRole,
+  ButtonCreateRole,
+  RolesContainer,
+  RoleRow,
+} from "./Roles.styles";
+// Assets
+import { ReactComponent as ChevronRightIcon } from "../../assets/chevron-right-solid.svg";
+import { ReactComponent as UsersIcon } from "../../assets/user-friends-solid.svg";
+import { ReactComponent as UserIcon } from "../../assets/single-user-solid.svg";
+import { ReactComponent as SearchIcon } from "../../assets/search-solid.svg";
+
+const Roles = ({ users, roles }) => {
+  const roleCounts = {};
+
+  for (const role of roles) {
+    roleCounts[role._id] = 0;
+  }
+
+  for (const user of users) {
+    for (const roleId of user.roles) {
+      roleCounts[roleId]++;
+    }
+  }
+
+  return (
+    <Container>
+      <Content>
+        <h1>Roles</h1>
+        <p>
+          Use roles to organize your server members and customize their
+          permissions
+        </p>
+        <DefaultPermissions>
+          <UsersIcon />
+          <div>
+            <h3>Default Permissions</h3>
+            <p>@everyone | applies to all server members</p>
+          </div>
+          <ChevronRightIcon />
+        </DefaultPermissions>
+        <CreateNewRole>
+          <input type="text" placeholder="Search Roles" />
+          <SearchIcon />
+          <ButtonCreateRole>Create Role</ButtonCreateRole>
+        </CreateNewRole>
+        <p>
+          Members use the color of the highest role they have on this list. Drag
+          roles to reorder them.
+        </p>
+        <RolesContainer>
+          <h4>Roles</h4>
+          <h4>Members</h4>
+        </RolesContainer>
+
+        {roles.map((role) => (
+          <RoleRow key={role._id}>
+            <h2>{role.name}</h2>
+            <div>
+              <span>
+                {roleCounts[role._id]} <UserIcon className="svg-icon" />
+              </span>
+            </div>
+          </RoleRow>
+        ))}
+      </Content>
+    </Container>
+  );
+};
+
+export default Roles;
---
+++ client/src/components/ServerSettingsModal/Roles.styles.js
@@ -0,0 +1,160 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  padding: 60px;
+  width: 90%;
+
+  h1 {
+    margin-bottom: 20px;
+    color: #fff;
+    font-size: 20px;
+    line-height: 24px;
+  }
+
+  > p {
+    margin-bottom: 16px;
+    color: #b9bbbe;
+    font-size: 14px;
+    line-height: 18px;
+  }
+
+  input {
+    background-color: #202225;
+    margin: 15px 15px 15px 0;
+    border: none;
+    min-width: 75%;
+    height: 34px;
+    border-radius: 4px;
+    padding: 10px;
+    color: #dcddde;
+    outline: none;
+  }
+`;
+
+export const DefaultPermissions = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: left;
+  justify-content: space-between;
+  background-color: #2f3136;
+  color: #b9bbbe;
+  border-radius: 4px;
+  padding: 16px 24px 16px 16px;
+  margin-bottom: 16px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #393d44;
+  }
+
+  > div {
+    margin-left: -420px;
+
+    p {
+      font-size: 12px;
+    }
+  }
+
+  h3 {
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const CreateNewRole = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    position: relative;
+    left: -20px;
+    background-color: #202225;
+    height: 34px;
+    padding: 6px;
+    border-radius: 4px;
+  }
+`;
+
+export const ButtonCreateRole = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #5865f2;
+  color: #fff;
+  height: 34px;
+  transition: background-color 0.17s ease, color 0.17s ease;
+  border: none;
+  border-radius: 3px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 16px;
+  padding: 2px 16px;
+  margin-left: 32px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #4752c4;
+  }
+
+  :active {
+    background-color: #3a42a0;
+  }
+`;
+
+export const RolesContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 50px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #b9bbbe;
+
+  h4 {
+    width: 400px;
+    font-size: 12px;
+    color: #b9bbbe;
+    text-transform: uppercase;
+  }
+`;
+
+export const RoleRow = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1px solid #b9bbbe;
+  height: 60px;
+  cursor: pointer;
+
+  h2 {
+    width: 400px;
+    font-size: 16px;
+    line-height: 20px;
+    font-weight: 500;
+    color: #fff;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  span {
+    font-size: 14px;
+    line-height: 20px;
+    color: #b9bbbe;
+  }
+
+  .svg-icon {
+    margin-left: 10px;
+    color: #b9bbbe;
+    width: 20px;
+    height: 20px;
+  }
+`;
